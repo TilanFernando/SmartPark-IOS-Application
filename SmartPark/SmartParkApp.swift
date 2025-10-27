ba39acd147c18229f4c0289f73a0896911fdeed8
TilanFernando <tilantrx@gmail.com>
2025-10-27 12:44:05 +0530

    Initial Commit

---
+++ SmartPark/SmartParkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartParkApp.swift
+//  SmartPark
+//
+//  Created by Dilshan Fernando on 2025-10-27.
+//
+
+import SwiftUI
+
+@main
+struct SmartParkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
